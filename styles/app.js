bb78c1e73fa2fe1d3ab6a41945b1dac49e2af799
Szlavicsek <semper576@gmail.com>
2019-01-16 21:25:26 +0100

    test commit

---
+++ styles/app.js
@@ -0,0 +1,9 @@
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  
+}
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  
+}
